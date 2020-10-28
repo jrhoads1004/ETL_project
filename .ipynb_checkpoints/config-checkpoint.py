9071b503895fa09260fa8dcc54f16e22989152fb
norewiler <n.orewiler1@gmail.com>
2020-10-27 19:29:41 -0500

    cleaned MainNotebook

--- .ipynb_checkpoints/config-checkpoint.py
+++ .ipynb_checkpoints/config-checkpoint.py
@@ -4,6 +4,6 @@ query = 'NextStory86@'
 
 pg_password = urllib.parse.quote(query)
 
-pg_password2 = 'NextStory86%40'
+# pg_password2 = 'NextStory86%40'
 
 api_key = "Do0NTQWQUFNatqd2ArzCFpWOxKMTEEpouIfT6262"
